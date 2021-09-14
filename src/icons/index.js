6178f7869ee45c11291b74918577a65b979603ba
Jackson <jacksonmain9@gmail.com>
2021-09-14 15:02:36 -0500

    used svgs as components instead of html img tags

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,3 +2,6 @@ export { default as Twitter } from "./Twitter";
 export { default as Instagram } from "./Instagram";
 export { default as Facebook } from "./Facebook";
 export { default as Pinterest } from "./Pinterest";
+export { default as FullyCustomizable } from "./FullyCustomizable";
+export { default as DetailedRecords } from "./DetailedRecords";
+export { default as BrandRecognition } from "./BrandRecognition";
