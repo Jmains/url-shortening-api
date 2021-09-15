a5448b230a85ee2a45277da37b03508ec77e78bd
Jackson <jacksonmain9@gmail.com>
2021-09-15 14:41:21 -0500

    added loader spinner pkg

--- src/components/shorten-url/ShortenUrl.js
+++ src/components/shorten-url/ShortenUrl.js
@@ -3,17 +3,45 @@ import LinkCard from "./LinkCard/LinkCard";
 import "./ShortenUrl.css";
 import cn from "classnames";
 
+// const getWeatherData = async () => {
+//   try {
+//     const res = await fetch(apiUrl);
+//     if (!res.ok) {
+//       throw new Error(`The status of the response is: ${res.status}`);
+//     }
+//     const data = await res.json();
+
+//     const dataArr = data.list.filter(isDesiredIndex).map((d) => {
+//       const { dt_txt, main, weather } = d;
+
+//       const icon = weather[0].icon.slice(0, -1);
+//       const min = main.temp_min.toFixed();
+//       const max = main.temp_max.toFixed();
+//       const date = new Date(dt_txt).toLocaleDateString('en-US', {
+//         weekday: 'short',
+//       });
+
+//       return { date, max, min, icon };
+//     });
+
+//     setData(dataArr);
+//     setIsLoading(false);
+//   } catch (err) {
+//     setError(err.message);
+//     setIsLoading(false);
+//   }
+// };
+
 const fetchShortLink = async (link) => {
-  const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`, {
-    method: "POST",
-  });
+  const cleanedLink = link.trim();
+  const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${cleanedLink}`);
   return await res.json();
 };
 
 export default function ShortenUrl() {
   const [link, setLink] = useState("");
   const [apiLinks, setApiLinks] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -32,23 +60,19 @@ export default function ShortenUrl() {
       return;
     }
 
-    setLoading(true);
+    setIsLoading(true);
+
     fetchShortLink(link)
-      .then((responseData) => {
-        if (responseData.ok) {
-          setApiLinks((prevApiLinks) => [responseData, ...prevApiLinks]);
-          sessionStorage.setItem(
-            "short_links",
-            JSON.stringify([responseData, ...apiLinks], null, 2)
-          );
-        } else {
-          setError(responseData.error);
-        }
+      .then((data) => {
+        if (!data.ok) throw new Error(data.error);
+        setApiLinks((prevData) => [data, ...prevData]);
+        sessionStorage.setItem("short_links", JSON.stringify([data, ...apiLinks], null, 2));
+        setIsLoading(false);
       })
       .catch((err) => {
-        console.log("something went wrong...", err);
+        setIsLoading(false);
+        setError(err.message);
       });
-    setLoading(false);
   };
 
   const handleLinkChange = (ev) => {
@@ -72,11 +96,11 @@ export default function ShortenUrl() {
             {error && <span className="short__form--error">{error}</span>}
           </div>
           <button className="shorten__btn" type="submit">
-            {loading ? "loading..." : "Shorten It!"}
+            {isLoading ? "loading..." : "Shorten It!"}
           </button>
         </form>
-        {apiLinks.length > 0 &&
-          apiLinks.map((link) => {
+        {apiLinks?.length > 0 &&
+          apiLinks?.map((link) => {
             return (
               <>
                 {link.ok && (
